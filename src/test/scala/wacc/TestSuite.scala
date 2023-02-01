73f13838b5ce94cebbda8e3c110269573d2e7a3c
Athena Critchley <ac2421@vertex29.doc.ic.ac.uk>
2023-02-01 01:31:54 +0000

    Updated the test suite to fix bugs, and ensure it goes through all directories

--- src/test/scala/wacc/TestSuite.scala
+++ src/test/scala/wacc/TestSuite.scala
@@ -9,26 +9,25 @@ class WaccTestSuite extends AnyFlatSpec {
   val rootPath = "src/test/scala/wacc/test_cases/"
 
   println("Initializing valid program test cases")
-  testDirectory(s"$rootPath/valid")
+  testDirectory(rootPath + "valid/", 0)
 
   println("Initializing invalid program test cases (syntax)")
-  testDirectory(s"$rootPath/invalid/syntaxErr")
+  testDirectory(rootPath + "invalid/syntaxErr/", 100)
 
   println("Initializing invalid program test cases (semantics)")
-  testDirectory(s"$rootPath/invalid/semanticErr")  
+  testDirectory(rootPath + "invalid/semanticErr/", 200)  
 
   def testDirectory(directoryPath: String, exitCode: Int) : Unit = {
-    val subdirectories = getDirectoryContents(directoryPath)
-    for (subdirectory <- subdirectories) {
-      val path = rootPath + subdirectory
-      val testFiles = getDirectoryContents(path)
-      // Loop through the test files and check our parser works
-      for (file <- testFiles) {
-          filePath = path + file
-          (s"Program '$filePath' ") should s"return an exit code of $exitCode" in {
-              assert(run(filePath) == exitCode)
-          }
-      }
+    // Extract all the files from the subdirectories (and their subdirectories)
+    val testFiles = getAllFiles(directoryPath)
+
+    // Loop through the test files and check our parser works
+    for (file <- testFiles) {
+        val filePath = directoryPath + file
+        println(filePath)
+        (s"Program '$filePath' ") should s"return an exit code of $exitCode" in {
+            assert(run(filePath) == exitCode)
+        }
     }
   }
   
@@ -38,6 +37,46 @@ class WaccTestSuite extends AnyFlatSpec {
     filePath.listFiles
             .map(_.getName)
             .toList
+  }
+
+  
+  // Because the Java isFile() function doesn't seem to work for .wacc files...
+  // We know there are only .wacc files in the directory, so this function works
+  def isAFile (fileName : String) : Boolean = {
+    if (fileName.length < 5) {
+      return false
+    } else {
+      return fileName.substring(fileName.length() - 5) == ".wacc"
+    }    
+  }
+
+  
+  def getAllFiles (path : String) : Array[String] = {
+    // Initialise an array, to which we add all .wacc files found
+    var files = Array[String]()// Array[String] = new Array()
+
+    // Get all subdirectories: array, basic, expressions, etc.
+    val subdirectories = getDirectoryContents(path)
+    for (subdirectory <- subdirectories) {
+      // Get all the files and subdirectories for each subdirectory
+      val subs = getDirectoryContents(path + subdirectory)
+      for (file <- subs) {
+        // If it's a file, add to the array of files
+        if (isAFile(file)) {
+          files = files :+ file
+        } else {
+          // If it's not a file then it's a directory - get all the files in the
+          // directory and append to the array of files
+          // Note that by observation, there are no further directories from
+          // this point
+          val finalFiles = getDirectoryContents(path + subdirectory + "/" + file)
+          for (f <- finalFiles) {
+            files = files :+ f
+          }
+        }
+      }
+    }
+    return files
   }
 
   def run (filePath: String) : Int = {
--- src/test/scala/wacc/validTestSuite.scala
+++ src/test/scala/wacc/validTestSuite.scala
@@ -1,75 +1,75 @@
-import org.scalatest.flatspec.AnyFlatSpec
-import scala.sys.process._
-import java.io.File
+// import org.scalatest.flatspec.AnyFlatSpec
+// import scala.sys.process._
+// import java.io.File
 
-class ValidProgram extends AnyFlatSpec {
+// class ValidProgram extends AnyFlatSpec {
 
-  // Get the root path, being the valid directory
-  val rootPath = "src/test/scala/wacc/test_cases/valid/"
+//   // Get the root path, being the valid directory
+//   val rootPath = "src/test/scala/wacc/test_cases/valid/"
 
-  // Extract all the files from the subdirectories (and their subdirectories)
-  val testFiles = getAllFiles(rootPath)
+//   // Extract all the files from the subdirectories (and their subdirectories)
+//   val testFiles = getAllFiles(rootPath)
 
-  for (file <- testFiles) {
-    test(file)
-  }
+//   for (file <- testFiles) {
+//     test(file)
+//   }
   
   
-  // Returns the contents of a directory as a List[String]
-  def getDirectoryContents (path : String) : List[String] = {
-    val filePath = new File(path)
-    filePath.listFiles
-            .map(_.getName)
-            .toList
-  }
+//   // Returns the contents of a directory as a List[String]
+//   def getDirectoryContents (path : String) : List[String] = {
+//     val filePath = new File(path)
+//     filePath.listFiles
+//             .map(_.getName)
+//             .toList
+//   }
 
-  // Because the Java isFile() function doesn't seem to work for .wacc files...
-  // We know there are only .wacc files in the directory, so this function works
-  def isAFile (fileName : String) : Boolean = {
-    if (fileName.length < 5) {
-      return false
-    } else {
-      return fileName.substring(fileName.length() - 5) == ".wacc"
-    }    
-  }
+//   // Because the Java isFile() function doesn't seem to work for .wacc files...
+//   // We know there are only .wacc files in the directory, so this function works
+//   def isAFile (fileName : String) : Boolean = {
+//     if (fileName.length < 5) {
+//       return false
+//     } else {
+//       return fileName.substring(fileName.length() - 5) == ".wacc"
+//     }    
+//   }
 
-  def getAllFiles (path : String) : Array[String] = {
-    // Initialise an array, to which we add all .wacc files found
-    var files = Array[String]()// Array[String] = new Array()
+//   def getAllFiles (path : String) : Array[String] = {
+//     // Initialise an array, to which we add all .wacc files found
+//     var files = Array[String]()// Array[String] = new Array()
 
-    // Get all subdirectories: array, basic, expressions, etc.
-    val subdirectories = getDirectoryContents(path)
-    for (subdirectory <- subdirectories) {
-      // Get all the files and subdirectories for each subdirectory
-      val subs = getDirectoryContents(path + subdirectory)
-      for (file <- subs) {
-        // If it's a file, add to the array of files
-        if (isAFile(file)) {
-          files = files :+ file
-        } else {
-          // If it's not a file then it's a directory - get all the files in the
-          // directory and append to the array of files
-          // Note that by observation, there are no further directories from
-          // this point
-          val finalFiles = getDirectoryContents(path + subdirectory + "/" + file)
-          for (f <- finalFiles) {
-            files = files :+ f
-          }
-        }
-      }
-    }
-    return files
-  }
+//     // Get all subdirectories: array, basic, expressions, etc.
+//     val subdirectories = getDirectoryContents(path)
+//     for (subdirectory <- subdirectories) {
+//       // Get all the files and subdirectories for each subdirectory
+//       val subs = getDirectoryContents(path + subdirectory)
+//       for (file <- subs) {
+//         // If it's a file, add to the array of files
+//         if (isAFile(file)) {
+//           files = files :+ file
+//         } else {
+//           // If it's not a file then it's a directory - get all the files in the
+//           // directory and append to the array of files
+//           // Note that by observation, there are no further directories from
+//           // this point
+//           val finalFiles = getDirectoryContents(path + subdirectory + "/" + file)
+//           for (f <- finalFiles) {
+//             files = files :+ f
+//           }
+//         }
+//       }
+//     }
+//     return files
+//   }
 
 
-  // Tests our parser works on a certain input file
-  // Return true if the test passed, false otherwise
-  def test (filePath : String) : Unit = {
-    ("Valid program " + filePath) should "return an exit code of 0" in {
-    val exitCode = Seq("./compile", filePath).!
-    // println("EXIT CODE: ", exitCode)
-    assert(exitCode == 0)
-    }
-  }
+//   // Tests our parser works on a certain input file
+//   // Return true if the test passed, false otherwise
+//   def test (filePath : String) : Unit = {
+//     ("Valid program " + filePath) should "return an exit code of 0" in {
+//     val exitCode = Seq("./compile", filePath).!
+//     // println("EXIT CODE: ", exitCode)
+//     assert(exitCode == 0)
+//     }
+//   }
 
-}
+// }
