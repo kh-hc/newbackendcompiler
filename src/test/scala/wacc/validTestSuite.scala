c5ddff0b621e1964e4c0afccbc20e0132ae097dd
Athena Critchley <ac2421@vertex29.doc.ic.ac.uk>
2023-02-01 01:20:10 +0000

    Fixed the valid test suite - can use to fix the full test suite
    Updated the valid test suite so it traverses all the subdirectories to find all the files in the test cases. TODO - use this to update the full test suite, so can test all the invalid cases as well

--- src/test/scala/wacc/validTestSuite.scala
+++ src/test/scala/wacc/validTestSuite.scala
@@ -6,17 +6,14 @@ class ValidProgram extends AnyFlatSpec {
 
   // Get the root path, being the valid directory
   val rootPath = "src/test/scala/wacc/test_cases/valid/"
-  // Get all the subdirectories in the valid folder
-  val subdirectories = getDirectoryContents(rootPath)
-  // Loop through the subdirectories and test each example within
-  for (subdirectory <- subdirectories) {
-    val path = rootPath + subdirectory
-    val testFiles = getDirectoryContents(path)
-    // Loop through the test files and check our parser works
-    for (file <- testFiles) {
-        test(path + file)
-    }
+
+  // Extract all the files from the subdirectories (and their subdirectories)
+  val testFiles = getAllFiles(rootPath)
+
+  for (file <- testFiles) {
+    test(file)
   }
+  
   
   // Returns the contents of a directory as a List[String]
   def getDirectoryContents (path : String) : List[String] = {
@@ -25,6 +22,45 @@ class ValidProgram extends AnyFlatSpec {
             .map(_.getName)
             .toList
   }
+
+  // Because the Java isFile() function doesn't seem to work for .wacc files...
+  // We know there are only .wacc files in the directory, so this function works
+  def isAFile (fileName : String) : Boolean = {
+    if (fileName.length < 5) {
+      return false
+    } else {
+      return fileName.substring(fileName.length() - 5) == ".wacc"
+    }    
+  }
+
+  def getAllFiles (path : String) : Array[String] = {
+    // Initialise an array, to which we add all .wacc files found
+    var files = Array[String]()// Array[String] = new Array()
+
+    // Get all subdirectories: array, basic, expressions, etc.
+    val subdirectories = getDirectoryContents(path)
+    for (subdirectory <- subdirectories) {
+      // Get all the files and subdirectories for each subdirectory
+      val subs = getDirectoryContents(path + subdirectory)
+      for (file <- subs) {
+        // If it's a file, add to the array of files
+        if (isAFile(file)) {
+          files = files :+ file
+        } else {
+          // If it's not a file then it's a directory - get all the files in the
+          // directory and append to the array of files
+          // Note that by observation, there are no further directories from
+          // this point
+          val finalFiles = getDirectoryContents(path + subdirectory + "/" + file)
+          for (f <- finalFiles) {
+            files = files :+ f
+          }
+        }
+      }
+    }
+    return files
+  }
+
 
   // Tests our parser works on a certain input file
   // Return true if the test passed, false otherwise
