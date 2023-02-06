eb44ff29398a9a1913a010c0a3797d2df7a51b00
Athena Critchley <ac2421@vertex29.doc.ic.ac.uk>
2023-02-06 11:59:01 +0000

    Added more test cases to the test suite - TODO: Brush this up

--- src/test/scala/wacc/ASTTestSuite.scala
+++ src/test/scala/wacc/ASTTestSuite.scala
@@ -22,8 +22,7 @@ class WaccTestSuite extends AnyFlatSpec {
   // Get the root path
   val validRootPath = "src/test/scala/wacc/test_cases/valid/"
 
-  // advanced subdirectory
-  ("./advanced/binarySortTree.wacc") should "be correctly parsed by our parser" in {
+  ("advanced/binarySortTree.wacc") should "be correctly parsed by our parser" in {
     val testFile = new File(validRootPath + "advanced/binarySortTree.wacc")
 
     // Format the test file for comparison
@@ -33,15 +32,87 @@ class WaccTestSuite extends AnyFlatSpec {
     val formattedParsedProgram = formatParserProgram(testFile)
 
     // println("formattedParsedProgram: " + formattedParsedProgram)
-    // println("\nformattedTest         : " + formattedTest)
-    // println("\n\n" + PrettyPrinters.prettyPrintStatement(statement) + "\n" + statement)
+    // println("\nformattedTest:          " + formattedTest)
 
     // Check that the parser works by comparing it to the test file
     assert(formattedTest == formattedParsedProgram)
   }
 
+  ("basic/exit/exit-1.wacc") should "be correctly parsed by our parser" in {
+    val testFile = new File(validRootPath + "basic/exit/exit-1.wacc")
+    val formattedTest = formatTestFile(testFile)
+    val formattedParsedProgram = formatParserProgram(testFile)
+    assert(formattedTest == formattedParsedProgram)
+  }
+
+  ("basic/skip/comment.wacc") should "be correctly parsed by our parser" in {
+    val testFile = new File(validRootPath + "basic/skip/comment.wacc")
+    val formattedTest = formatTestFile(testFile)
+    val formattedParsedProgram = formatParserProgram(testFile)
+    assert(formattedTest == formattedParsedProgram)
+  }
+
+  ("expressions/andExpr.wacc") should "be correctly parsed by our parser" in {
+    val testFile = new File(validRootPath + "expressions/andExpr.wacc")
+    val formattedTest = formatTestFile(testFile)
+    val formattedParsedProgram = formatParserProgram(testFile)
+    assert(formattedTest == formattedParsedProgram)
+  }
+
+  ("function/nested_functions/fibonacciFullRec.wacc") should "be correctly parsed by our parser" in {
+    val testFile = new File(validRootPath + "function/nested_functions/fibonacciFullRec.wacc")
+    val formattedTest = formatTestFile(testFile)
+    val formattedParsedProgram = formatParserProgram(testFile)
+    assert(formattedTest == formattedParsedProgram)
+  }
+
+  // ("function/simple_functions/fibonacciFullRec.wacc") should "be correctly parsed by our parser" in {
+  //   val testFile = new File(validRootPath + "function/simple_functions/asciiTable.wacc")
+  //   val formattedTest = formatTestFile(testFile)
+  //   val formattedParsedProgram = formatParserProgram(testFile)
+  //   assert(formattedTest == formattedParsedProgram)
+  // }
+
+  ("if/if1.wacc") should "be correctly parsed by our parser" in {
+    val testFile = new File(validRootPath + "if/if1.wacc")
+    val formattedTest = formatTestFile(testFile)
+    val formattedParsedProgram = formatParserProgram(testFile)
+    assert(formattedTest == formattedParsedProgram)
+  }
+
+  // ("IO/print/hashInProgram.wacc") should "be correctly parsed by our parser" in {
+  //   val testFile = new File(validRootPath + "IO/print/hashInProgram.wacc")
+  //   val formattedTest = formatTestFile(testFile)
+  //   val formattedParsedProgram = formatParserProgram(testFile)
+  //   assert(formattedTest == formattedParsedProgram)
+  // }
+
+  ("IO/read/echoBigInt.wacc") should "be correctly parsed by our parser" in {
+    val testFile = new File(validRootPath + "IO/read/echoBigInt.wacc")
+    val formattedTest = formatTestFile(testFile)
+    val formattedParsedProgram = formatParserProgram(testFile)
+    assert(formattedTest == formattedParsedProgram)
+  }
+
+  // ("IO/IOLoop.wacc") should "be correctly parsed by our parser" in {
+  //   val testFile = new File(validRootPath + "IO/IOLoop.wacc")
+  //   val formattedTest = formatTestFile(testFile)
+  //   val formattedParsedProgram = formatParserProgram(testFile)
+  //   assert(formattedTest == formattedParsedProgram)
+  // }
+
+  // ("pairs/checkRefPair.wacc") should "be correctly parsed by our parser" in {
+  //   val testFile = new File(validRootPath + "pairs/checkRefPair.wacc")
+  //   val formattedTest = formatTestFile(testFile)
+  //   val formattedParsedProgram = formatParserProgram(testFile)
+  //   assert(formattedTest == formattedParsedProgram)
+  // }
+
+
+  ////////////// Helper functions
+
   def discardWhitespace(text : String) : String = {
-    text.replace(" ", "")
+    text.replace(" ", "").replace("\n", "").replace("\t", "")
   }
 
   // Gets all text from a file without lines or whitespace
@@ -78,9 +149,24 @@ class WaccTestSuite extends AnyFlatSpec {
     parsedProgramLines = parsedProgramLines + PrettyPrinters.prettyPrintStatement(statement)
     while (parsedProgramLines.charAt(parsedProgramLines.length - 1) == ';') {
       parsedProgramLines = parsedProgramLines.slice(0, parsedProgramLines.length - 2)
-      //println("\"" + endChar + "\"")
     }
     parsedProgramLines = parsedProgramLines + "end"
-    discardWhitespace(parsedProgramLines).replace("\n", "")
+    discardWhitespace(parsedProgramLines)
   }
+
+  // def parsedProgramIsCorrect(formattedTest : String, formattedParsedProgram : String) = {
+  //   if (formattedTest.length >= formattedParsedProgram.length) {
+  //     compare(formattedTest, formattedParsedProgram)
+  //   } else {
+  //     compare(formattedParsedProgram, formattedTest)
+  //   }
+  // }
+
+  // def compare(stringLonger : String, stringShorter : String) {
+  //   for (i <- 0 to stringLonger.length) {
+  //     val charLonger = stringLonger.charAt(i)
+  //     val charShorter = stringLonger.charAt(i)
+  //     while (charLonger == )
+  //   }
+  // }
 }
