f6ca720826e0d05b17a9e275f9cf09bd91481c92
evienut <evelyn.nutton1@btinternet.com>
2023-02-07 10:05:39 +0000

    Add labels for errors

--- src/main/scala/wacc/Parser.scala
+++ src/main/scala/wacc/Parser.scala
@@ -12,46 +12,69 @@ object parser {
     import implicits.implicitSymbol
     import abstractSyntaxTree._
     import parsley.expr.{precedence, SOps, InfixL, InfixN, Prefix, Atoms, chain}
+    import parsley.errors.combinator._
 
     private val identifier: Parsley[Identifier] = Identifier(IDENT)
 
     private val arrayElem: Parsley[ArrayElem] = ArrayElem(identifier, many("[" *> expression <* "]"))
+        .label("Array element")
 
-    private lazy val pairElemType: Parsley[PairElemType] = "pair" #> NestedPair <|> PairElemTypeT(tiepe)
+    private lazy val pairElemType: Parsley[PairElemType] = ("pair" #> NestedPair <|> PairElemTypeT(tiepe))
+        .label("Pair element type")
     
     private val baseType: Parsley[BaseType] = ("int" #> IntT 
         <|> "bool" #> BoolT
         <|> "char" #> CharT
         <|> "string" #> StringT)
+        .label("Base Type")
+        .explain("Valid base types are int, bool, char and string.")
 
-    private val pairType: Parsley[PairType] = PairType("pair" *> "(" *> pairElemType <* ",", pairElemType <* ")")
+    private val pairType: Parsley[PairType] = PairType("pair" *> "(" *> pairElemType.label("fst") <* ",", pairElemType.label("snd") <* ")")
+        .label("Pair Type")
 
     private val tiepe: Parsley[Type] = chain.postfix(baseType <|> pairType, ArrayType <# "[]")
+        .label("Type")
 
-    private lazy val func: Parsley[FunctionUnit] = (attempt(ParamFunc(tiepe, identifier, "(" *> paramList <* ")", ("is" *> statement <* "end").filter(stats => endsInRet(stats)))
-        <|> NiladicFunc(tiepe, identifier, ("(" *> ")" *> "is" *> statement <* "end").filter(stats => endsInRet(stats)))))
+    private lazy val func: Parsley[FunctionUnit] = (attempt(ParamFunc(tiepe.label("type"), identifier.label("identifier"),
+            ("(" *> paramList.label("parameter list") <* ")"), 
+            ("is" *> statement.label("statement") <* "end").filter(stats => endsInRet(stats)))
+            <|> NiladicFunc(tiepe.label("type"), identifier.label("identifier"), 
+            ("(" *> ")" *> "is" *> statement.label("statement") <* "end")
+                .filter(stats => endsInRet(stats)))))
+                .label("Function")
+                .explain("A function requires a return type, identifier, 0 or more parameters, and a statement." +
+                  "It must end in a return.")
 
-    private lazy val paramList: Parsley[ParamList] = ParamList(sepBy(param, ","))
+    private lazy val paramList: Parsley[ParamList] = ParamList(sepBy(param, (",")))
+        .label("Parameter list")
 
     private val param: Parsley[Param] = Param(tiepe, identifier)
+        .label("Parameter")
 
-    private lazy val statement: Parsley[StatementUnit] = SeqStat(sepBy1(atomStatement, ";"))
+    private lazy val statement: Parsley[StatementUnit] = SeqStat(sepBy1(atomStatement, (";")))
+        .label("Statement")
+        .explain("A statement can be either a list of statements or a singular statement.")
 
     private lazy val atomStatement: Parsley[StatementUnit] = ("skip" #> SkipStat
-        <|> AssignStat(tiepe, identifier, "=" *> rValue)
-        <|> ReassignStat(lValue, "=" *> rValue)
-        <|> ReadStat("read" *> lValue)
-        <|> FreeStat("free" *> expression)
-        <|> ReturnStat("return" *> expression)
-        <|> ExitStat("exit" *> expression)
-        <|> PrintStat("print" *> expression)
-        <|> PrintlnStat("println" *> expression)
-        <|> IfStat("if" *> expression, "then" *> statement, "else" *> statement <* "fi")
-        <|> WhileStat("while" *> expression, "do" *> statement <* "done")
-        <|> ScopeStat("begin" *> statement <* "end")
+        <|> AssignStat(tiepe, identifier, "=" *> rValue).label("assignment")
+        <|> ReassignStat(lValue, "=" *> rValue).label("reassignment")
+        <|> ReadStat("read" *> lValue).label("read")
+        <|> FreeStat("free" *> expression).label("free")
+        <|> ReturnStat("return" *> expression).label("return")
+        <|> ExitStat("exit" *> expression).label("exit")
+        <|> PrintStat("print" *> expression).label("print")
+        <|> PrintlnStat("println" *> expression).label("println")
+        <|> IfStat("if" *> expression, "then" *> statement, "else" *> statement <* "fi").label("if statement")
+        <|> WhileStat("while" *> expression, "do" *> statement <* "done").label("while")
+        <|> ScopeStat("begin" *> statement <* "end").label("new scope")
     )
+        .label("Atomic Statement")
+        .explain("An atomic statement can be 'skip', 'assignment', 'reassignment'," +
+          "'read', 'free', 'return', 'exit', 'print', 'println', 'if', 'while', or " +
+          "a new scope definition with 'begin'")
 
     private lazy val lValue: Parsley[Lvalue] = arrayElem <|> (pairElem <|> identifier)
+        .label("Left Value")
 
     private lazy val rValue: Parsley[Rvalue] = (expression
         <|> arrayLiteral
@@ -59,37 +82,51 @@ object parser {
         <|> pairElem
         <|> attempt(ParamCall("call" *> identifier <* "(", argList <* ")"))
         <|> attempt(NiladicCall("call" *> identifier <* "(" <* ")")))
+        .label("Right Value")
 
     private val pairElem: Parsley[PairElem] = (PairElemFst("fst" *> lValue)
         <|> PairElemSnd("snd" *> lValue))
+        .label("Pair element")
+
         
     private val arrayLiteral: Parsley[ArrayLiteral] = ArrayLiteral("[" *> sepBy(expression, ",") <* "]")
+        .label("Array Literal")
 
-    private val atomicExpression: Parsley[Expr0] = (IntExpr(INT)
-        <|> StrExpr(STRING)
-        <|> CharExpr(CHAR)
-        <|> BoolExpr("true" #> true)
-        <|> BoolExpr("false" #> false)
-        <|> "null" #> PairLiteral
-        <|> arrayElem
-        <|> identifier
-        <|> ParenExpr("(" *> expression <* ")"))
+    private val atomicExpression: Parsley[Expr0] = (IntExpr(INT).label("int")
+        <|> StrExpr(STRING).label("string")
+        <|> CharExpr(CHAR).label("char")
+        <|> BoolExpr("true" #> true).label("true")
+        <|> BoolExpr("false" #> false).label("false")
+        <|> ("null" #> PairLiteral).label("pair literal")
+        <|> arrayElem.label("array element")
+        <|> identifier.label("identifier")
+        <|> ParenExpr("(" *> expression <* ")").label("parenthesised expression"))
+        .label("Atomic expression")
+        .explain("An atomic expression can be a string, char, boolean, pair literal, array element," +
+          "identifier, or a paranthesised expression.")
 
     private val expression: Parsley[Expr] = 
-        precedence(SOps(InfixL)(Or <# "||") +:
-            SOps(InfixL)(And <# "&&") +:
-            SOps(InfixN)(NotEqual <# "!=", Equal <# "==") +:
-            SOps(InfixN)(LessOrEqualThan <# "<=", LessThan <# "<",
-                GreaterOrEqualThan <# ">=", GreaterThan <# ">") +:
-            SOps(InfixL)(Add <# "+", Sub <# "-") +:
-            SOps(InfixL)(Mul <# "*", Div <# "/", Mod <# "%") +:
-            SOps(Prefix)(NotOp <# "!", NegateOp <# NEGATE , LenOp <# "len",
-                OrdOp <# "ord", ChrOp <# "chr") +:
+        precedence(SOps(InfixL)((Or <# "||").label("OR")) +:
+            SOps(InfixL)((And <# "&&").label("AND")) +:
+            SOps(InfixN)((NotEqual <# "!=").label("NOT EQUAL"), (Equal <# "==").label("EQUAL")) +:
+            SOps(InfixN)((LessOrEqualThan <# "<=").label("LESS OR EQUAL"), (LessThan <# "<").label("LESS"),
+                (GreaterOrEqualThan <# ">=").label("GREATER OR EQUAl"), (GreaterThan <# ">").label("GREATER")) +:
+            SOps(InfixL)((Add <# "+").label("AND"), (Sub <# "-").label("SUB")) +:
+            SOps(InfixL)((Mul <# "*").label("MUL"), (Div <# "/").label("DIV"), (Mod <# "%").label("MOD")) +:
+            SOps(Prefix)((NotOp <# "!").label("NOT"), (NegateOp <# NEGATE).label("NEGATE") , (LenOp <# "len").label("LEN"),
+                (OrdOp <# "ord").label("ORD"), (ChrOp <# "chr").label("CHR")) +:
             Atoms(atomicExpression))
+        .label("Expression")
+        .explain("An expression can be a binary operation, unary operation, or atomic expression." +
+          "Binary operations are infix notated and can be '||', '&&', '!=', '==', '<=', '<','>=', '>'" +
+          "'+', '-', '*', '/', '%'. Unary operations are prefix notated and can be '!', '-', 'len', " +
+          "'ord', 'chr'.")
 
     private val argList: Parsley[ArgList] = ArgList(sepBy1(expression, ","))
+        .label("Argument list")
 
     private val program: Parsley[WACCprogram] = fully(WACCprogram("begin" *> many(func), statement <* "end"))
+        .label("WACC program")
 
     def parse(input: File) = program.parseFromFile(input).get
 
