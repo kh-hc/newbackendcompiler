8ffe1fe9196790e4fe2bdc314d7dc25cd82260f6
Tom <88183789+TomFitchie@users.noreply.github.com>
2023-02-17 13:25:43 +0000

    Continued work on the translator

--- src/main/scala/wacc/ASTtoAbsAss.scala
+++ src/main/scala/wacc/ASTtoAbsAss.scala
@@ -26,49 +26,99 @@ class AbstractTranslator {
 
     def translateStat(stat: StatementUnit): List[Instruction] = stat match {
         case SkipStat => List.empty
-        case AssignStat(t, id, value) => 
+        case AssignStat(t, id, value) => {
+            val (rightIntermediate, instrR) = translateRvalue(value, stat.symbolTable.get)
+            return instrR ++ List(UnaryOperation(Assign, rightIntermediate, Stored(stat.symbolTable.get.lookupRecursiveId(id.id))))
+        }
         case ReassignStat(left, right) => {
-            val (leftIntermediate, instrL) = translateLvalue(left)
-            val (rightIntermediate, instrR) = translateRvalue(right)
+            val (leftIntermediate, instrL) = translateLvalue(left, stat.symbolTable.get)
+            val (rightIntermediate, instrR) = translateRvalue(right, stat.symbolTable.get)
+            return instrL ++ instrR ++ List(UnaryOperation(Assign, rightIntermediate, leftIntermediate))
         }
         case ReadStat(value) => {
-            val (dest, instr) = translateLvalue(value)
+            val (dest, instr) = translateLvalue(value, stat.symbolTable.get)
             return instr ++ List(InbuiltFunction(Read, dest))
         }
         case FreeStat(expr) => {
             val intermediate = getNewIntermediate
-            return translateExp(expr, intermediate) ++ List(InbuiltFunction(Free, intermediate))
+            return translateExp(expr, intermediate, stat.symbolTable.get) ++ List(InbuiltFunction(Free, intermediate))
         }
         case ReturnStat(expr) => {
             val intermediate = getNewIntermediate
-            return translateExp(expr, intermediate) ++ List(InbuiltFunction(Return, intermediate))
+            return translateExp(expr, intermediate, stat.symbolTable.get) ++ List(InbuiltFunction(Return, intermediate))
         }
         case ExitStat(expr) => {
             val intermediate = getNewIntermediate
-            return translateExp(expr, intermediate) ++ List(InbuiltFunction(Exit, intermediate))
+            return translateExp(expr, intermediate, stat.symbolTable.get) ++ List(InbuiltFunction(Exit, intermediate))
         }
         case PrintStat(expr) => {
             val intermediate = getNewIntermediate
-            return translateExp(expr, intermediate) ++ List(InbuiltFunction(Print, intermediate))
+            return translateExp(expr, intermediate, stat.symbolTable.get) ++ List(InbuiltFunction(Print, intermediate))
         }
         case PrintlnStat(expr) => {
             val intermediate = getNewIntermediate
-            return translateExp(expr, intermediate) ++ List(InbuiltFunction(Println, intermediate))
+            return translateExp(expr, intermediate, stat.symbolTable.get) ++ List(InbuiltFunction(Println, intermediate))
         }
-        case IfStat(cond, ifStat, elseStat) => 
-        case WhileStat(cond, body) => 
+        case IfStat(cond, ifStat, elseStat) => {
+            val intermediate = getNewIntermediate
+            val conditions = translateExp(cond, intermediate, stat.symbolTable.get)
+            return List(IfInstruction(Conditional(intermediate, conditions), translateStat(ifStat), translateStat(elseStat)))
+        }
+        case WhileStat(cond, body) =>  {
+            val intermediate = getNewIntermediate
+            val conditions = translateExp(cond, intermediate, stat.symbolTable.get)
+            return List(WhileInstruction(Conditional(intermediate, conditions), translateStat(body)))
+        }
         case ScopeStat(body) => translateStat(body)
         case SeqStat(statements) => statements.map(translateStat).flatten
     }
 
-    def translateLvalue(value: Lvalue): (Value, List[Instruction]) = value match {
-        case PairElemFst(pair) => 
-        case PairElemSnd(pair) => 
-        case Identifier(id) => 
-        case ArrayElem(id, position) => 
+    def translateRvalue(value: Rvalue, st: SymbolTable): (Value, List[Instruction]) = value match {
+        case ArrayLiteral(value) => {
+            val array = getNewIntermediate
+            // Create array, go through indices and add the translated values
+            return (array, List.empty)
+        }
+        case NewPair(exprLeft, exprRight) => {
+            val pair = getNewIntermediate
+            // Create pair, add values to fst and snd
+            return (pair, List.empty)
+        }
+        case Call(id, args) => {
+            val returnVal = getNewIntermediate
+            // Translate all args, use call
+            return (returnVal, List.empty)
+        }
+        case lvalue: Lvalue => translateLvalue(lvalue, st)
+        case expr: Expr => {
+            val intermediate = getNewIntermediate
+            return (intermediate, translateExp(expr, intermediate, st))
+        }
     }
 
-    def translateExp(ast: Expr, dest: Stored): List[Instruction] = {
+    def translateLvalue(value: Lvalue, st: SymbolTable): (Value, List[Instruction]) = value match {
+        case PairElemFst(pairToDeref) => {
+            val (pair, instr) = translateLvalue(pairToDeref, st)
+            return (PairAccess(Snd, pair), instr)
+        }
+        case PairElemSnd(pairToDeref) => {
+            val (pair, instr) = translateLvalue(pairToDeref, st)
+            return (PairAccess(Snd, pair), instr)
+        }
+        case Identifier(id) => (Stored(st.lookupRecursiveId(id)), List.empty)
+        case ArrayElem(id, position) => {
+            var positions: List[Value] = List.empty
+            var instructions: List[Instruction] = List.empty
+            for (pos <- position) {
+                val inter = getNewIntermediate
+                positions = positions :+ inter
+                instructions = instructions ::: translateExp(pos, inter, st) 
+            }
+            return (ArrayAccess(positions, Stored(st.lookupRecursiveId(id.id))), instructions)
+        }
+    }
+
+    def translateExp(ast: Expr, dest: Stored, st: SymbolTable): List[Instruction] = {
         return List.empty
     }
 }
--- src/main/scala/wacc/AssemblyStructure.scala
+++ src/main/scala/wacc/AssemblyStructure.scala
@@ -16,19 +16,20 @@ object assemblyAbstractStructure {
     case class BinaryOperation(operator: AssemblyOperator, src1: Value, src2: Value, dest: Value) extends Instruction
     case class UnaryOperation(operator: AssemblyOperator, src: Value, dest: Value) extends Instruction
     case class InbuiltFunction(operator: AssemblyOperator, src: Value) extends Instruction
-    case class FunctionCall(functionName: String, args: List[Value]) extends Instruction
+    case class FunctionCall(functionName: String, args: List[Value], returnTo: Value) extends Instruction
 
-    case class IfInstruction(condition: Conditional, ifInstructions: List[Instruction], elseInstructions: List[Instruction])
-    case class WhileInstruction(condition: Conditional, body: List[Instruction])
+    case class IfInstruction(condition: Conditional, ifInstructions: List[Instruction], elseInstructions: List[Instruction]) extends Instruction
+    case class WhileInstruction(condition: Conditional, body: List[Instruction]) extends Instruction
 
-    case class Conditional(operator: AssemblyOperator, src1: Value, src2: Value)
+    case class Conditional(value: Value, conditions: List[Instruction])
 
     sealed trait Value
     case class Stored(id: String) extends Value
     case class Immediate(value: Int) extends Value
     case class StringLiteral(value: String) extends Value
-    case class PairAccess(pos: PairPos, pair: Stored) extends Value
-    case class ArrayAccess(pos: List[Int], array: Stored) extends Value
+
+    case class PairAccess(pos: PairPos, pair: Value) extends Value
+    case class ArrayAccess(pos: List[Value], array: Stored) extends Value
 
     sealed trait PairPos
     case object Fst extends PairPos
@@ -46,4 +47,6 @@ object assemblyAbstractStructure {
     case object Free extends AssemblyOperator
     case object Return extends AssemblyOperator
     case object Exit extends AssemblyOperator
+    case object ArrayCreate extends AssemblyOperator // src: Length, dst: Addr
+    case object PairCreate extends AssemblyOperator // src: Addr
 }
