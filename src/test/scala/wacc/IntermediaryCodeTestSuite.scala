e6aa837ed0440969e994f1a814288a4d70ff9204
Athena Critchley <ac2421@vertex29.doc.ic.ac.uk>
2023-02-20 13:02:46 +0000

    Bug fixed in getIntermediaryCode (goes through semantic analyser too. Tests added)

--- src/test/scala/wacc/IntermediaryCodeTestSuite.scala
+++ src/test/scala/wacc/IntermediaryCodeTestSuite.scala
@@ -6,42 +6,87 @@ import assemblyAbstractStructure._
 import java.io.File
 
 class IntermediaryCodeTestSuite extends AnyFlatSpec {
-    
-    // Create a translator for the test suite to use
-    val translator = new AbstractTranslator()
 
     // Used for some tests - the root path
     val validRootPath = "src/test/scala/wacc/test_cases/valid/"
 
     ("A simple skip program") should "have intermediary code correctly produced by our translator" in {
+        // Create a translator
+        val translator = new AbstractTranslator()
         // Create a simple WACC program containing a single skip instruction
         val skip = WACCprogram(Nil, SkipStat)(0, 0)
         // Translate the skip instruction
-        val intermediateCode = translator.translate(skip)
+        val intermediaryCode = translator.translate(skip)
         // Define what we expect as output
         val expectedCode = Program(Nil, Nil)
-        assert(intermediateCode == expectedCode)
+        assert(intermediaryCode == expectedCode)
     }
 
     ("basic/skip/skip.wacc") should "have intermediary code correctly produced by our translator" in {
         val testFile = validRootPath + "basic/skip/skip.wacc"
-        // Get the intermediate code
-        val intermediateCode = getIntermediateCode(testFile)
+        // Get the intermediary code
+        val intermediaryCode = getintermediaryCode(testFile)
         val expectedCode = Program(Nil, Nil)
-        assert(intermediateCode == expectedCode)
+        assert(intermediaryCode == expectedCode)
     }
 
-    // ("basic/exit/exit-1.wacc") should "have intermediary code correctly produced by our translator" in {
-    //     val testFile = validRootPath + "basic/exit/exit-1.wacc"
-    //     val intermediateCode = getIntermediateCode(testFile)
-    //     println("Intermediate code: ", intermediateCode)
-    //     val expectedCode = Program(Nil, Nil) 
-    //     assert(intermediateCode == expectedCode)
+    ("basic/skip/comment.wacc") should "be correctly parsed by our parser" in {
+        val testFile = validRootPath + "basic/skip/comment.wacc"
+        val intermediaryCode = getintermediaryCode(testFile)
+        val expectedCode = Program(Nil, Nil)
+        assert(intermediaryCode == expectedCode)
+    }
+
+    ("basic/exit/exit-1.wacc") should "have intermediary code correctly produced by our translator" in {
+        val testFile = validRootPath + "basic/exit/exit-1.wacc"
+        val intermediaryCode = getintermediaryCode(testFile)
+        val expectedCode = Program(List(InbuiltFunction(A_Exit, Stored("1"))), List())
+        assert(intermediaryCode == expectedCode)
+    }
+
+    // ("expressions/divExpr.wacc") should "have intermediary code correctly produced by our translator" in {
+    //     val testFile = validRootPath + "expressions/divExpr.wacc"
+    //     val intermediaryCode = getintermediaryCode(testFile)
+    //     println("intermediary code: ", intermediaryCode)
+
+    //     // Expected code for main
+    //     var expectedMain = List[Instruction]()
+    //     // int x = 5
+    //     expectedMain = expectedMain ++ List(UnaryOperation(A_Assign, Stored("1"), Stored("x0")))
+    //     // int y = 3
+    //     expectedMain = expectedMain ++ List(UnaryOperation(A_Assign, Stored("3"), Stored("y0")))
+    //     // println x / y
+    //     // expectedMain = expectedMain ++ List(InbuiltFunction(A_Println))
+
+    //     val expectedCode = Program(expectedMain, Nil)
+    //     assert(intermediaryCode == expectedCode)
     // }
 
-    def getIntermediateCode(filePath : String) : Program = {
-        // Get the intermediate code
+    // ("expressions/andExpr.wacc") should "be correctly parsed by our parser" in {
+    //     val testFile = validRootPath + "expressions/andExpr.wacc"
+    //     val intermediaryCode = getintermediaryCode(testFile)
+    //     val expectedCode = Program(Nil, Nil)
+    //     assert(intermediaryCode == expectedCode)
+    // }
+
+//    ("while/fibonacciFullIt.wacc") should "have intermediary code correctly produced by our translator" in {
+//         val testFile = validRootPath + "while/fibonacciFullIt.wacc"
+//         val conditions = translateExp(cond, intermediate, stat.symbolTable.get)
+//         val intermediaryCode = getintermediaryCode(testFile)
+//         println("intermediary code: ", intermediaryCode)
+//         val expectedCode = Program(List(), List()) // [A_Assign, A_Neg, A_Exit])
+//          List(WhileInstruction(Conditional(intermediate, conditions), translateStat(body)))
+//         assert(intermediaryCode == expectedCode)
+//     }
+
+    def getintermediaryCode(filePath : String) : Program = {
+        // Get the intermediary code
         val ast = parser.parse(new File(filePath)).get
+        val sa = new SemanticAnalyzer("test suite")
+        sa.analyzeProgram(ast)
+        // Create a translator
+        val translator = new AbstractTranslator()
+        // Translate the ast into intermediary code
         return translator.translate(ast)
     }
 }
